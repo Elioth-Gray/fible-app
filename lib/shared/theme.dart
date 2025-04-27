cd81025f62c25a70fc46acd8ab30d31035550c57
Pilemon <barimbingpilemon@gmail.com>
2025-04-27 18:18:28 +0700

    add-theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,44 @@
+import "package:flutter/material.dart";
+import 'package:google_fonts/google_fonts.dart';
+
+Color primaryColor = const Color(0xFF4993FA);
+Color secondaryColor = const Color(0xFFAED4FF);
+Color tertiaryColor = const Color(0xFF9DC6FF);
+Color primaryProgressBarColor = const Color(0xFF0060E7);
+Color secondaryProgressBarColor = const Color(0xFF4993FA);
+Color tertiaryProgressBarColor = const Color(0xFFABCEFF);
+Color whiteColor = const Color(0xFFFFFFFF);
+Color floatingActionButtonColor = const Color(0xFF0D4B71);
+Color blackNavbarColor = const Color(0xFF424242);
+Color greenColor = const Color(0xFF3EA343);
+Color redColor = const Color(0xFFD84C4D);
+Color greyColor = const Color(0xFF787878);
+Color blackColor = const Color(0xFF19191B);
+
+TextStyle blackTextStyle = GoogleFonts.openSans(
+  color: blackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.openSans(
+  color: whiteColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.openSans(
+  color: greyColor,
+);
+
+TextStyle greenTextStyle = GoogleFonts.openSans(
+  color: greenColor,
+);
+
+TextStyle refTextStyle = GoogleFonts.openSans(
+  color: redColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
